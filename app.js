c91e928275d0cb1d8ef7c39b6ac1713eb27cde75
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-05-09 23:19:19 +0530

    install nodenom

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 //apt-get install node
 //apt-get install npm
 //npm init
+//npm install nodemon
+//run nodenom => nodenom app.js
 const validator=require('validator'); //npm install validator
+const chalk=require('chalk');
 const fs=require('fs'); 
 const add=require('./util.js');
 const add1=add(5,6);
 console.log(add1);
 
-fs.writeFileSync('node.txt','I am node js i am gonna eat you')
+fs.writeFileSync('node.txt','I am node js i am gonna eat you');
 
 
 console.log(validator.isEmail('bat48655@gmail.com'));
 console.log(validator.isURL('www.google.com'));
-
+const greenMsg=chalk.red('Error!');
+console.log(greenMsg);
