4c9ae552d6f74e8d8453455867e4574d87a8318a
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-05-12 20:37:52 +0530

    command line args

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 //npm install nodemon
 //run nodenom => nodenom app.js
 const validator=require('validator'); //npm install validator
+const yargs=require('yargs');
 const chalk=require('chalk');
 const fs=require('fs'); 
 const add=require('./util.js');
@@ -17,3 +18,7 @@ console.log(validator.isEmail('bat48655@gmail.com'));
 console.log(validator.isURL('www.google.com'));
 const greenMsg=chalk.red('Error!');
 console.log(greenMsg);
+
+
+console.log(process.argv[0]);//commend line argument
+console.log(yargs.argv);
