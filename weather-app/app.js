3aa1b38ebc8cf5ee5fcb27190065fac4afb80f3c
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-06-01 13:17:33 +0530

    in weather app include latitude and longitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,16 @@
-const request = require('request');
+const request=require('request');
 
-const url="http://api.weatherstack.com/current?access_key=334ae39f7298e2962e8a400062089eb8&query=kolkata";
+const locationURL="https://api.mapbox.com/geocoding/v5/mapbox.places/kolkata.json?access_token=pk.eyJ1IjoiYmF0NDg2NTUiLCJhIjoiY2thdzU4czVrMGZoczJxbzdlZng0djRpdiJ9.f8enBBAheKJv5TCvWeqooA";
+request({url:locationURL,json:true},(error,response)=>{
+    const lat=response.body.features[0].center[0];
+    const lon=response.body.features[0].center[1];
+    
+    console.log(lat+" "+lon);
+});
 
-request({ url: url, json:true },(error,response)=>{
-    console.log(response.body.current);
+const cuntryName="kolkata";
+const weatherURL="http://api.weatherstack.com/current?access_key=334ae39f7298e2962e8a400062089eb8&query="+cuntryName;
+
+request({ url: weatherURL, json:true },(error,response)=>{
+    console.log(response.body.current.temperature);
 });
